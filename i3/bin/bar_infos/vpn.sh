a8dc37e3ef0ac997914a379a94358e4a84a4f717
Rodskin <rod.gilles@gmail.com>
2021-06-10 17:17:30 +0200

    ajout de configuration et alias pour docker

--- i3/bin/bar_infos/vpn.sh
+++ i3/bin/bar_infos/vpn.sh
@@ -19,22 +19,22 @@ myvpn_on() {
   echo -n "},"
 }
 
-#myvmy_on() {
-#  local bg=$bg_inactive
-#  local icon=$icon_vpn_off
-#  vpn_status=`ip a | grep 'tun0'`
-#  if [ ! -z "$vpn_status" ]; then
-#    bg=$bg_vpn
-#    icon=$icon_vpn_on
-#  fi
+myvmy_on() {
+  local bg=$bg_inactive
+  local icon=$icon_vpn_off
+  vpn_status=`ip a | grep 'tun0'`
+  if [ ! -z "$vpn_status" ]; then
+    bg=$bg_vpn
+    icon=$icon_vpn_on
+  fi
 
-  #separator $bg $bg_separator_previous # background left previous block
-#  bg_separator_previous=$bg
-#  echo -n "{"
-#  echo -n "\"name\":\"id_vmy\","      
-#  echo -n "\"full_text\":\" ${icon} VMY \","
-#  echo -n "\"color\":\"$color_vpn\","
-#  echo -n "\"background\":\"$bg\","
-#  common
-#  echo -n "},"
-#}
+ #separator $bg $bg_separator_previous # background left previous block
+  bg_separator_previous=$bg
+  echo -n "{"
+  echo -n "\"name\":\"id_vmy\","      
+  echo -n "\"full_text\":\" ${icon} VMY \","
+  echo -n "\"color\":\"$color_vpn\","
+  echo -n "\"background\":\"$bg\","
+  common
+  echo -n "},"
+}
