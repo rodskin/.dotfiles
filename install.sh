2459a1d0221c68fb87651f6ec2cc97f280b79e04
Rodskin <rod.gilles@gmail.com>
2021-07-15 15:08:08 +0200

    ajout des snaps à installer

--- install.sh
+++ install.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 source colors.sh
+source install_functions.sh
+
 echo -e "${green}************************************************${default}"
 echo -e "${green}* INSTALATION DES LOGICIELS${default}"
 echo -e "${green}************************************************${default}"
 while read in
 do  
-    echo -e "${yellow} INSTALLATION DE ${in}:${default}"
+    dot_mes_install ${in}
+    #echo -e "${yellow} INSTALLATION DE ${in}:${default}"
     sudo apt install -y "$in"
 done < programs_to_install.txt
+
+while read in
+do  
+    dot_mes_install ${in}
+    #echo -e "${yellow} INSTALLATION DE ${in}:${default}"
+    sudo snap install "$in"
+done < snaps_install.txt
 
 echo -e "${green}************************************************${default}"
 echo -e "${green}* INSTALATION DE DOCKER-COMPOSE${default}"
