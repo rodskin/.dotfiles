40e5e49fca5d69074562624865a99a4310990616
Rodskin <rod.gilles@gmail.com>
2021-10-19 12:47:33 +0200

    ajout de l'install docker dans un dossier séparé

--- install.sh
+++ install.sh
@@ -19,13 +19,7 @@ do
     sudo snap install "$in"
 done < snaps_install.txt
 
-echo -e "${green}************************************************${default}"
-echo -e "${green}* INSTALATION DE DOCKER-COMPOSE${default}"
-echo -e "${green}************************************************${default}"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo usermod -aG docker "$USER"
-
+for f in scripts_install/*.sh ; do source $f ; done
 
 echo -e "${green}************************************************${default}"
 echo -e "${green}* CREATION DES LIENS SYMBOLIQUES${default}"
