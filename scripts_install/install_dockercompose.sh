e5f06bfb9db95dedd53f877b252c6ea139ea1a3e
Rodskin <rod.gilles@gmail.com>
2021-10-19 14:41:28 +0200

    maj installeur

--- scripts_install/install_dockercompose.sh
+++ scripts_install/install_dockercompose.sh
@@ -5,3 +5,4 @@ echo -e "${green}************************************************${default}"
 DC_LATEST_VERSION=`curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'`
 sudo curl -L "https://github.com/docker/compose/releases/download/$DC_LATEST_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+docker-compose -v
