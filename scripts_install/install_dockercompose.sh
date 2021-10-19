12dfdf20745f9ae79195d1d05e91521731be7f30
Rodskin <rod.gilles@gmail.com>
2021-10-19 14:23:21 +0200

    separation installation docker et docker compose

---
+++ scripts_install/install_dockercompose.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo -e "${green}************************************************${default}"
+echo -e "${green}* INSTALATION DE DOCKER-COMPOSE${default}"
+echo -e "${green}************************************************${default}"
+DC_LATEST_VERSION=`curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'`
+sudo curl -L "https://github.com/docker/compose/releases/download/$DC_LATEST_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
