d6dc14eeff0c1a4c06f6a3a0f1743468b66da241
Rodskin <rod.gilles@gmail.com>
2021-11-16 11:13:47 +0100

    ajout de la gestion epic games

---
+++ i3status/epic_add_game_and_open.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+game_name=`python3 ~/.config/i3status/epic.py`
+game_url=`python3 ~/.config/i3status/epic_url.py`
+
+
+if [ -n "$game_url" ]; then
+    echo $game_name >> ~/.dotfiles/i3_infos/epic_list.txt
+    xdg-open https://www.epicgames.com/store/en-US/p/"$game_url" > /dev/null &
+fi
